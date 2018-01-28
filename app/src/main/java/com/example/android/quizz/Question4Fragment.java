bfe8bd12c57d193176bf3346cf04eaaa23b28fe1
Hanndroid <hannah.m.evans4@gmail.com>
2018-01-28 21:26:37 +0000

    added question 4 to 5th fragment

---
+++ app/src/main/java/com/example/android/quizz/Question4Fragment.java
@@ -0,0 +1,33 @@
+package com.example.android.quizz;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Question4Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_question_4, container, false);
+
+        TextView tv = (TextView) v.findViewById(R.id.tvFragSecond);
+        tv.setText(getArguments().getString("msg"));
+
+        return v;
+    }
+
+    public static Question4Fragment newInstance(String text) {
+
+        Question4Fragment f = new Question4Fragment();
+        Bundle b = new Bundle();
+        b.putString("msg", text);
+
+        f.setArguments(b);
+
+        return f;
+    }
+
+}
