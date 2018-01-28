9a7a0666e4cf2fd6e1d6b3e65081c94111e94ab9
Hanndroid <hannah.m.evans4@gmail.com>
2018-01-28 19:31:22 +0000

    Initial commit

---
+++ app/src/main/java/com/example/android/quizz/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.quizz;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+
+import com.example.android.quizz.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
+
+
+
