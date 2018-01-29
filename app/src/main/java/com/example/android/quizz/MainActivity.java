ea2e882a4bea07722067720cdd15b5f0a80e8194
Hanndroid <hannah.m.evans4@gmail.com>
2018-01-29 20:15:50 +0000

    removed title bar messages

--- app/src/main/java/com/example/android/quizz/MainActivity.java
+++ app/src/main/java/com/example/android/quizz/MainActivity.java
@@ -9,50 +9,50 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
-        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
-    }
-
-    private class MyPagerAdapter extends FragmentPagerAdapter {
-
-        public MyPagerAdapter(FragmentManager fm) {
-            super(fm);
+            ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+            pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
         }
 
-        @Override
-        public Fragment getItem(int pos) {
-            switch (pos) {
-                default:
-                    return IntroFragment.newInstance("intro");
-                case 0:
-                    return IntroFragment.newInstance("Intro");
-                case 1:
-                    return Question1Fragment.newInstance("question 1");
-                case 2:
-                    return Question2Fragment.newInstance("question 2");
-                case 3:
-                    return Question3Fragment.newInstance("question 3");
-                case 4:
-                    return Question4Fragment.newInstance("question 4");
-                case 5:
-                    return Question5Fragment.newInstance("question 5");
-                case 6:
-                    return Question6Fragment.newInstance("question 6");
-                case 7:
-                    return Question7Fragment.newInstance("question 7");
-                case 8:
-                    return ResultsFragment.newInstance("results");
+        private class MyPagerAdapter extends FragmentPagerAdapter {
+
+            public MyPagerAdapter(FragmentManager fm) {
+                super(fm);
+            }
+
+            @Override
+            public Fragment getItem(int pos) {
+                switch (pos) {
+                    default:
+                        return IntroFragment.newInstance("");
+                    case 0:
+                        return IntroFragment.newInstance("");
+                    case 1:
+                        return Question1Fragment.newInstance("");
+                    case 2:
+                        return Question2Fragment.newInstance("");
+                    case 3:
+                        return Question3Fragment.newInstance("");
+                    case 4:
+                        return Question4Fragment.newInstance("");
+                    case 5:
+                        return Question5Fragment.newInstance("");
+                    case 6:
+                        return Question6Fragment.newInstance("");
+                    case 7:
+                        return Question7Fragment.newInstance("");
+                    case 8:
+                        return ResultsFragment.newInstance("");
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 9;
             }
         }
-
-        @Override
-        public int getCount() {
-            return 9;
-        }
-    }
 }
