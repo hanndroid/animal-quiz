5be1646ae365495d0f36687bf92c821734cb75b7
Hanndroid <hannah.m.evans4@gmail.com>
2018-01-29 20:14:39 +0000

    removed title bar text

--- app/src/main/java/com/example/android/quizz/Question1Fragment.java
+++ app/src/main/java/com/example/android/quizz/Question1Fragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class Question1Fragment extends Fragment {
